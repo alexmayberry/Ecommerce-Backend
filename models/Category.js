33e1fc23ba79ad3ecf532a0746159110d9cd99c9
alexmayberry <alexclimbs@gmail.com>
2022-09-07 17:29:06 -0700

    cleaning git workingtree

---
+++ models/Category.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require('sequelize');
+const { DataTypes } = require('sequelize/types/index.js');
+
+const sequelize = require('../config/connection.js');
+
+class Category extends Model {}
+
+Category.init(
+  {
+    // define columns
+  //   `id`
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    } 
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
+);
+
+module.exports = Category;
